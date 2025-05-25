129dba455306bf494b01293880accd244fe5fab4
samiksha275 <agarwalsamiksha88@gmail.com>
2025-05-26 03:06:19 +0530

    Enabled the button fpr analysis

--- helper/src/App.js
+++ helper/src/App.js
@@ -3,10 +3,23 @@ import { useState } from 'react';
 
 function App(){
   const [query, setQuery] = useState('');
+  const [isAnalyzing, setIsAnalysis] = useState(false);
+  const [analysisResult, setAnalysisResult] = useState('');
+
+  const handleAnalysis = () => {
+    setIsAnalysis(true);
+    setAnalysisResult('');
+
+    setTimeout(()=>{
+      setIsAnalysis(false);
+      setAnalysisResult("mock analysis with ${query}");
+    },3000);
+  }
+
   return (
     <div class="interface">
       <textarea id="inputText" placeholder="Enter your query here..." value={query} onChange={(e)=>setQuery(e.target.value)}></textarea>
-      <button id="analyzeBtn">Analyze</button>
+      <button id="analyzeBtn" onClick={handleAnalysis}>Analyze</button>
   </div>
   );
 }
